7fe0e22fb8cff4a032bc0811f18164b0dd4f49fa
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2024-07-25 14:45:56 +0900

    [level 1] Title: 최소직사각형, Time: 54.01 ms, Memory: 101 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        
+        for(int i = 0; i < sizes.length; i++) {
+            for(int j = 0; j < sizes[i].length-1; j++) {
+                if(sizes[i][j+1] > sizes[i][j]) {
+                	int temp = sizes[i][j];
+                    sizes[i][j] = sizes[i][j+1];
+                    sizes[i][j+1] = temp;
+                }
+            }
+        }
+        for(int i = 0; i < sizes.length; i++) {
+            
+            System.out.print(Arrays.toString(sizes[i]));
+            
+            System.out.println();
+        }
+        
+        int max1 = 0;
+        int max2 = 0;
+        for(int i = 0; i < sizes.length; i++) {            
+            if(max1 < sizes[i][0])
+                max1 = sizes[i][0];
+            if(sizes[i][1] > max2)
+                max2 = sizes[i][1];            
+        }
+        answer = max1 * max2;
+        return answer;
+    }
+}
