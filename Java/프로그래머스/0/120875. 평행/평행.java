d0eeebd79fe2aefed549e83493126ca9fa6892c3
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2024-08-01 15:06:00 +0900

    [level 0] Title: 평행, Time: 0.04 ms, Memory: 73.5 MB -BaekjoonHub

--- Java/프로그래머스/0/120875. 평행/평행.java
+++ Java/프로그래머스/0/120875. 평행/평행.java
@@ -1,17 +1,15 @@
 class Solution {
     public int solution(int[][] dots) {
         int answer = 0;
-        // 기울기 구하기
-        
-		if((double)(dots[0][1] - dots[1][1]) / (dots[0][0] - dots[1][0]) == (double)(dots[2][1] - dots[3][1]) / (dots[2][0] - dots[3][0]))
+        if(function(dots[0], dots[1]) == function(dots[2], dots[3]))
             answer = 1;
-        else if((double)(dots[0][1] - dots[2][1])/(dots[0][0] - dots[2][0]) == (double)(dots[1][1] - dots[3][1]) / (dots[1][0] - dots[3][0]))
+        else if(function(dots[0], dots[2]) == function(dots[1], dots[3]))
             answer = 1;
-        else if((double)(dots[0][1] - dots[3][1]) / (dots[0][0] - dots[3][0]) == (double)(dots[1][1] - dots[2][1]) / (dots[1][0] - dots[2][0]))
+        else if(function(dots[0], dots[3]) == function(dots[1], dots[2]))
             answer = 1;
-        else
-            answer = 0;
-        
         return answer;
+    }
+    double function(int[] dot1, int[] dot2) {
+        return (double)(dot2[1] - dot1[1]) / (dot2[0] - dot1[0]);
     }
 }
