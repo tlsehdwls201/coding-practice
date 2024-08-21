f5e377b1a39cc0917e9dfbfd020cce6dd78dd27b
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2024-08-21 17:12:13 +0900

    [level 0] Title: 다항식 더하기, Time: 16.40 ms, Memory: 82.4 MB -BaekjoonHub

--- Java/프로그래머스/0/120863. 다항식 더하기/README.md
+++ Java/프로그래머스/0/120863. 다항식 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 다항식 더하기 - 120863 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120863#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120863) 
 
 ### 성능 요약
 
-메모리: 78.5 MB, 시간: 13.89 ms
+메모리: 82.4 MB, 시간: 16.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 21일 16:16:29
+2024년 08월 21일 17:12:09
 
 ### 문제 설명
 
--- Java/프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
+++ Java/프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
@@ -1,48 +1,35 @@
-import java.util.*;
 class Solution {
     public String solution(String polynomial) {
-       String[] parts = polynomial.split(" ");
-        System.out.println(Arrays.toString(parts));
-        int x = 0;  // 'x' 항의 계수를 저장할 변수
-        int num = 0; // 상수를 저장할 변수
-
-        // 부분 문자열 배열을 순회하여 'x' 항과 상수 항을 구분
-        for (String s : parts) {
-            if (s.contains("x")) { // 'x' 항일 때
-                s = s.replace("x", ""); // 'x'를 제거하여 숫자만 남김
-                if (s.length() == 0) // 'x'만 있는 경우 계수는 1
+        String answer = "";
+        String str[] = polynomial.split(" ");
+        int x = 0;
+        int n = 0;
+        for(int i = 0; i < str.length; i++) {
+            if(str[i].contains("x")) {
+                String s = str[i].replace("x", "");
+                if(s.length() == 0)
                     x++;
-                else // 'x'와 숫자가 함께 있는 경우 숫자를 정수로 변환하여 계수에 추가
+                else
                     x += Integer.parseInt(s);
-            } else if (!s.equals("+")) { // '+'가 아닌 나머지 문자열은 상수 항
-                num += Integer.parseInt(s); // 상수 값을 정수로 변환하여 누적
+            }
+            else if(!str[i].equals("+")) {
+                n += Integer.parseInt(str[i]);
             }
         }
+       
         
-        String result; // 최종 결과 문자열
-        
-        // 모든 항이 0인 경우
-        if (x == 0 && num == 0) {
-            return "0";
+            if(x == 1) {
+                answer = "x";
+            }
+            else
+            	answer = x + "x";
+    	
+        if(n != 0) {
+            if(x == 0)
+                answer = "" + n;
+        	else
+            	answer += " + " + n;
         }
-        
-        // 'x' 항이 1인 경우
-        if (x == 1) {
-            result = "x";
-        } else { // 'x' 항이 1보다 큰 경우
-            result = x + "x";
-        }
-        
-        // 상수 항이 0인 경우
-        if (num == 0) {
-            return result;
-        } else if (x == 0) { // 'x' 항이 없는 경우
-            return "" + num;
-        } else { // 'x' 항과 상수 항이 모두 있는 경우
-            result += " + " + num;
-        }
-        
-        return result; // 최종 결과 반환
+        return answer;
     }
-
-    }
+}
