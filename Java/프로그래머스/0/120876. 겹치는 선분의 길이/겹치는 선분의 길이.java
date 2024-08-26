6ad20c529c104360ee84c1899e7bb1f7589665f1
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2024-08-26 18:29:05 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 4.01 ms, Memory: 73 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] lines) {
+       	int answer = 0;
+        Map<Integer, Integer> m = new HashMap<>();
+        for(int i =0; i < lines.length; i++) {
+            for(int j = lines[i][0]; j < lines[i][1]; j++) {
+                m.put(j, m.getOrDefault(j, 0) + 1);
+            }
+        }
+        System.out.println(m);
+        int count = 0;
+      	
+        for(Map.Entry<Integer, Integer> e : m.entrySet()) {
+            if(e.getValue() != 1)
+                count++;
+        }
+        answer = count;
+        return answer;
+    }
+}
