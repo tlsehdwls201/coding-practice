f727f5f208447df713f745fed00c96790dff485e
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2024-09-06 20:34:34 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 18.80 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String str[] = s.split("");
+        if(str.length % 2 == 0) {
+            answer += str[str.length/2 - 1] + str[str.length/2];
+        }
+        else
+            answer += str[str.length/2];
+        return answer;
+    }
+}
