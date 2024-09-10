73dbf6aaf9ae83f6a2269b348cb1cb3193220dde
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2024-09-10 14:06:13 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 16.24 ms, Memory: 75 MB -BaekjoonHub

--- Java/프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/README.md
+++ Java/프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내 마음대로 정렬하기 - 12915 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915) 
 
 ### 성능 요약
 
-메모리: 72.5 MB, 시간: 1.19 ms
+메모리: 75 MB, 시간: 16.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 10일 14:01:46
+2024년 09월 10일 14:06:09
 
 ### 문제 설명
 
--- Java/프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ Java/프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,19 +1,16 @@
 import java.util.*;
-
 class Solution {
     public String[] solution(String[] strings, int n) {
-        // 배열을 정렬하면서 두 가지 기준 적용: n번째 문자, 그 다음은 사전순 정렬
-        Arrays.sort(strings, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                // n번째 문자가 같은 경우, 전체 문자열을 사전순으로 정렬
-                if (s1.charAt(n) == s2.charAt(n)) {
-                    return s1.compareTo(s2);
-                }
-                // n번째 문자를 기준으로 정렬
-                return Character.compare(s1.charAt(n), s2.charAt(n));
-            }
-        });
-        return strings;
+        String[] answer = {};
+        List<String> l = new ArrayList<>();
+        for(int i = 0; i < strings.length; i++) {
+            l.add(strings[i].charAt(n) + strings[i]);
+        }
+        Collections.sort(l);
+        answer = new String[l.size()];
+        for(int i = 0; i < l.size(); i++) {
+            answer[i] = l.get(i).substring(1, l.get(i).length());
+        }
+        return answer;
     }
 }
