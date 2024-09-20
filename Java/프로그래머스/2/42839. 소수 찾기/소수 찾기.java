1f61a6e258441b0dadbecf15d8c7ad02ecf870fa
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2024-09-20 15:40:57 +0900

    [level 2] Title: 소수 찾기, Time: 95.65 ms, Memory: 97.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Solution {
+    Set<Integer> s = new HashSet<>();
+    public int solution(String numbers) {
+        int answer = 0;
+        boolean[] visited = new boolean[numbers.length()];
+        for(int i = 0; i < numbers.length(); i++) {
+            dfs(numbers, i+1, "", 0, visited);
+        }
+        return s.size();
+    }
+    
+    //dfs
+    private void dfs(String numbers, int range, String now, int idx, boolean visit[]) {
+        if(range == idx) {
+            int num = Integer.parseInt(now);
+            if(isPrime(num)) s.add(num);
+            return;
+        }
+        
+        for(int i = 0; i < numbers.length(); i++) {
+            if(visit[i]) continue;
+            
+            visit[i] = true;
+            dfs(numbers, range, now+numbers.charAt(i), idx+1, visit);
+            visit[i] = false;
+        }    
+            return;
+        
+    }
+    //소수 판별하는 메소드 생성
+    private boolean isPrime(int n) {
+        boolean result = true;
+        if(n < 2) result = false;
+        if(n == 2) result = true;
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(n % i == 0)
+                result = false;
+        }
+        return result;
+    }
+}
