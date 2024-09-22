c122afcb27f7edceeb1bd4ddd3e99a36207dca80
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2024-09-22 23:17:00 +0900

    [level 2] Title: 최솟값 만들기, Time: 7.01 ms, Memory: 52.2 MB -BaekjoonHub

--- Java/프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ Java/프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,15 +1,15 @@
 import java.util.*;
+// 우선순위 큐 알고리즘
+
 class Solution {
     public int solution(int []A, int []B) {
         int answer = 0;
 		Queue<Integer> aq = new PriorityQueue<>();
-       	Queue<Integer> bq = new PriorityQueue<>(Collections.reverseOrder());
+       	Queue<Integer> bq = new PriorityQueue<>(Collections.reverseOrder()); // 내림차순
         for(int i = 0; i < A.length; i++) {
             aq.add(A[i]);
             bq.add(B[i]);
         }
-        
-        
         
         for(int i = 0; i < A.length; i++) {
             answer += aq.poll() * bq.poll();
