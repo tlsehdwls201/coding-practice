7aa94a8e545fa7a98c8bbbb6d9b8371fbadac6ec
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2024-10-03 14:33:10 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int a = Integer.parseInt(n_str);
+        answer = Integer.toString(a);
+        return answer;
+    }
+}
