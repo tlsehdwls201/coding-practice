1b8713d34112bd68de414a729bce0e381406dfbd
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2024-12-31 20:24:03 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.03 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        int answer = 0;
+        
+        for(int i = 0; i < date1.length; i++) {
+            if(date1[i] > date2[i]) {
+                break;
+                
+            }
+            if(date1[i] < date2[i]) {
+                answer = 1;
+            }
+            
+        }
+        return answer;
+    }
+}
