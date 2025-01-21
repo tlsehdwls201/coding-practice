80ee4ccef40c1288f6d995ce541717cfccfb9db6
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2025-01-21 19:03:11 +0900

    [Silver II] Title: DNA 비밀번호, Time: 316 ms, Memory: 24260 KB -BaekjoonHub

--- Java/백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ Java/백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -2,42 +2,42 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static char c_arr[] = {'A', 'C', 'G', 'T'};
     static int check_arr[];
     static int arr[];
-    static int checkSecret = 0; // 각 인덱스 별, arr의 값과 동일하거나 큰 개수의 모음
+    static int checkIndex;
     public static void main(String[] args) throws IOException{
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(r.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int result = 0; // 정답 값
+        char dna[] = r.readLine().toCharArray();
+        st = new StringTokenizer(r.readLine());
         check_arr = new int[4];
         arr = new int[4];
-        char A[] = r.readLine().toCharArray();
-        //int checkSecret = 0; // 각 인덱스 별, arr의 값과 동일하거나 큰 개수의 모음
-
-        st = new StringTokenizer(r.readLine());
+        checkIndex = 0;
         for(int i = 0; i < check_arr.length; i++) {
             check_arr[i] = Integer.parseInt(st.nextToken());
-            if(check_arr[i] == 0)
-                checkSecret++;
+            if(check_arr[i] == 0) checkIndex++;
         }
 
-        for(int i = 0; i < M; i++) { // 부분문자열 처음 받을 때 세팅
-            Add(A[i]);
+        int result = 0;
+
+        for(int i = 0; i < M; i++) { // 초기 부분 문자열 길이까지...
+            Add(dna[i]);
+
         }
 
-        if(checkSecret == 4) result++;
+        if(checkIndex == 4) result++;
 
-        //슬라이딩 윈도우
         for(int i = M; i < N; i++) {
             int j = i - M;
-            Add(A[i]);
-            Remove(A[j]);
-            if(checkSecret == 4) result++;
+            Add(dna[i]);
+            Remove(dna[j]);
+            if(checkIndex == 4) result++;
         }
+
+
         w.write(result + "\n");
 
 
@@ -47,21 +47,21 @@ public class Main {
     }
 
     private static void Remove(char c) {
-        switch (c) {
+        switch(c) {
             case 'A':
-                if(arr[0] == check_arr[0]) checkSecret--;
+                if(arr[0] == check_arr[0])checkIndex--;
                 arr[0]--;
                 break;
             case 'C':
-                if(arr[1] == check_arr[1]) checkSecret--;
+                if(arr[1] == check_arr[1])checkIndex--;
                 arr[1]--;
                 break;
             case 'G':
-                if(arr[2] == check_arr[2]) checkSecret--;
+                if(arr[2] == check_arr[2])checkIndex--;
                 arr[2]--;
                 break;
             case 'T':
-                if(arr[3] == check_arr[3]) checkSecret--;
+                if(arr[3] == check_arr[3])checkIndex--;
                 arr[3]--;
                 break;
         }
@@ -71,19 +71,19 @@ public class Main {
         switch(c) {
             case 'A':
                 arr[0]++;
-                if(arr[0] == check_arr[0]) checkSecret++;
+                if(arr[0] == check_arr[0]) checkIndex++;
                 break;
             case 'C':
                 arr[1]++;
-                if(arr[1] == check_arr[1]) checkSecret++;
+                if(arr[1] == check_arr[1]) checkIndex++;
                 break;
             case 'G':
                 arr[2]++;
-                if(arr[2] == check_arr[2]) checkSecret++;
+                if(arr[2] == check_arr[2]) checkIndex++;
                 break;
             case 'T':
                 arr[3]++;
-                if(arr[3] == check_arr[3]) checkSecret++;
+                if(arr[3] == check_arr[3]) checkIndex++;
                 break;
         }
     }
