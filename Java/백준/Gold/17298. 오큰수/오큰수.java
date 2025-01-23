bf1d97023284df9dc0aa5ee1c7cd82dee5f4358e
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2025-01-23 23:29:46 +0900

    [Gold IV] Title: 오큰수, Time: 1020 ms, Memory: 184072 KB -BaekjoonHub

--- Java/백준/Gold/17298. 오큰수/오큰수.java
+++ Java/백준/Gold/17298. 오큰수/오큰수.java
@@ -2,50 +2,56 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(r.readLine());
-		Stack<Integer> stack = new Stack<>();
-		int arr[] = new int[n];
-		int result[] = new int[n];
-		 StringTokenizer st = new StringTokenizer(r.readLine());
-		for(int i = 0; i < n; i++) {
-			int a = Integer.parseInt(st.nextToken());
-			arr[i] = a;
-			if(stack.isEmpty()) {
-				stack.push(i);
-				continue;
-			}
-			if(arr[stack.peek()] < arr[i]) {
-				while(!stack.isEmpty()) {
-					if(arr[stack.peek()] >= arr[i]) {
-						
-						break;
-					}
-					else
-						result[stack.pop()] = arr[i];
-					
-				}
-				stack.push(i);
-			}
-			else {
-				stack.push(i);
-			}
-			
-			
-		}
-		for(int i = 0; i < n; i++) {
-			result[stack.pop()] = -1;
-			if(stack.isEmpty())
-				break;
-		}
-		for(int i = 0; i < result.length; i++) {
-			w.write((result[i]) + " ");
-		}
-		w.flush();
-		w.close();
-		r.close();
-	}
+        int N = Integer.parseInt(r.readLine());
+        StringTokenizer st = new StringTokenizer(r.readLine());
+        Stack<Integer> stack = new Stack<>();
+        int arr[] = new int[N];
+        int result[] = new int[N];
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if(stack.isEmpty()) {
+                stack.push(i);
+                continue;
+            }
+
+            if(arr[stack.peek()] < arr[i]) {
+                int size = stack.size();
+                for(int j = 1; j <= size; j++) {
+                    if(arr[stack.peek()] >= arr[i]) break;
+                    result[stack.pop()] = arr[i];
+                    
+                }
+                stack.push(i);
+            }
+
+            else {
+                stack.push(i);
+            }
+        }
+
+        if(!stack.isEmpty()) {
+            int size = stack.size();
+            for(int i = 0; i< size; i++) {
+                result[stack.pop()] = -1;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < result.length; i++) {
+            sb.append(result[i] + " ");
+        }
+
+        w.write(sb.toString().trim());
+
+
+
+        w.flush();
+        w.close();
+        r.close();
+    }
+
 }
