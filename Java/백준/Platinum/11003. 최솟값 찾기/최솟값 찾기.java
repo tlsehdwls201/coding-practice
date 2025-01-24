f4cff126aa6b3d9537df01406b6396fbf4837c9b
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2025-01-24 16:33:35 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2288 ms, Memory: 619756 KB -BaekjoonHub

--- Java/백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ Java/백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -2,41 +2,42 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(r.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(r.readLine());
-        Deque<Node> deque = new LinkedList<>(); // 덱 자료구조 이용
+        Deque<A> deque = new LinkedList<>();
+        List<Integer> l = new ArrayList<>();
+
         for(int i = 0; i < N; i++) {
             int now = Integer.parseInt(st.nextToken());
-
             while(!deque.isEmpty() && deque.getLast().value > now) {
                 deque.removeLast();
             }
+            deque.addLast(new A(i,now));
 
-            deque.addLast(new Node(i, now));
-
-            if(deque.getFirst().index <= i - M) {
+            if(i - M >= deque.getFirst().index) {
                 deque.removeFirst();
             }
-
             w.write(deque.getFirst().value + " ");
 
+
         }
+
 
 
         w.flush();
         w.close();
         r.close();
     }
-    static class Node {
+    static class A {
         private int index;
         private int value;
 
-        public Node(int i, int v) {
+        public A(int i, int v) {
             this.index = i;
             this.value = v;
         }
