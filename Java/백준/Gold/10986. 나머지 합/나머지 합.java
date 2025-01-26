203a53b66e623ec1793bcee21ea30126a7528b8b
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2025-01-26 17:24:31 +0900

    [Gold III] Title: 나머지 합, Time: 500 ms, Memory: 128860 KB -BaekjoonHub

--- Java/백준/Gold/10986. 나머지 합/나머지 합.java
+++ Java/백준/Gold/10986. 나머지 합/나머지 합.java
@@ -2,45 +2,43 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(r.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+
         long arr[] = new long[N];
         long sum_arr[] = new long[N];
-        String s[] = r.readLine().split(" ");
-
+        long count = 0;
+        st = new StringTokenizer(r.readLine());
         for(int i = 0; i < N; i++) {
-            arr[i] = Long.parseLong(s[i]);
-            if(i == 0) sum_arr[i] = arr[i];
-            else sum_arr[i] = sum_arr[i-1] + arr[i];
+            arr[i] = Long.parseLong(st.nextToken());
+            if(i == 0) {
+                sum_arr[i] = arr[i] % M;
+                if(sum_arr[i] == 0) count++;
+                continue;
+            }
+            sum_arr[i] = (sum_arr[i-1] + arr[i]) % M;
+
+            if(sum_arr[i] == 0) count++;
         }
 
-
-
-        long count[] = new long[M];
-        long cnt = 0;
+        long cnt[] = new long[M];
         for(int i = 0; i < N; i++) {
-            int index = (int)(sum_arr[i] % M);
-            if(index == 0) cnt++;
-
-            count[index]++;
-
+            cnt[(int)sum_arr[i]]++;
         }
 
         for(int i = 0; i < M; i++) {
-            if(count[i] > 1) {
-                cnt += (count[i] * (count[i] - 1) / 2);
-            }
+            count += (cnt[i] * (cnt[i] - 1) / 2);
         }
-        w.write(cnt + "\n");
-
+        w.write(count + "\n");
 
 
         w.flush();
         w.close();
         r.close();
     }
+
 }
