383d8f23744f930b630de6522a8f8526474133a5
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2025-01-26 17:32:58 +0900

    [Silver V] Title: 수들의 합 5, Time: 160 ms, Memory: 15920 KB -BaekjoonHub

--- Java/백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ Java/백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -1,38 +1,40 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int N = Integer.parseInt(r.readLine());
 
-        long sum = 1; // 1부터 투 포인터가 이동해가며 sum에 값을 저장해 나가기 때문에 1로 초기화
-        int start_idx = 1;
-        int end_idx = 1;
-        int cnt = 1; // N값 포함한 경우의 수를 저장하는 변수라 이를 1로 지정
-        while(end_idx != N) {
-            if(sum < N) {
 
-                end_idx++;
-                sum += end_idx;
+        int start = 1;
+        int end = 1;
+        int sum = start;
+        int result = 0;
+
+        while(end <= N) {
+
+            if(sum < N) {
+                end++;
+                sum += end;
             }
             else if(sum > N) {
-                sum -= start_idx;
-                start_idx++;
+                sum -= start;
+                start++;
+
             }
             else {
-                end_idx++;
-                sum += end_idx;
-                cnt++;
+                end++;
+                sum += end;
+                result++;
             }
         }
-        w.write(cnt+"\n");
+        w.write(result + "\n");
 
 
         w.flush();
         w.close();
         r.close();
     }
+
 }
