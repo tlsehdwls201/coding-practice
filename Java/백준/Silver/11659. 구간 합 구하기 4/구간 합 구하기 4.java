c27fe4279e028c52730b51210a3a1ec0732ccb53
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2025-01-26 16:24:16 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 656 ms, Memory: 52804 KB -BaekjoonHub

--- Java/백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ Java/백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -2,44 +2,37 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(r.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        String s[] = new String[N];
-        s = r.readLine().split(" ");
-        int arr[] = new int[N];
-        int sum_arr[] = new int[N];
-        for(int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(s[i]);
-            if(i == 0) {
+        st = new StringTokenizer(r.readLine());
+        int arr[] = new int[N+1];
+        int sum_arr[] = new int[N+1];
+        for(int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if(i == 1) {
                 sum_arr[i] = arr[i];
+                continue;
             }
-            else {
-                sum_arr[i] = sum_arr[i-1] + arr[i];
-            }
+            sum_arr[i] = sum_arr[i-1] + arr[i];
         }
 
+        int result = 0;
         for(int i = 0; i < M; i++) {
-            int sum = 0;
-            String str[] = r.readLine().split(" ");
-            int a = Integer.parseInt(str[0]);
-            int b = Integer.parseInt(str[1]);
+            st = new StringTokenizer(r.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            if(a < 2) {
-                sum = sum_arr[b-1];
-            }
-            else
-                sum = sum_arr[b-1] - sum_arr[a-2];
-
-            w.write(sum + "\n");
+            result = sum_arr[b] - sum_arr[a-1];
+            w.write(result + "\n");
         }
-
 
         w.flush();
         w.close();
         r.close();
     }
+
 }
