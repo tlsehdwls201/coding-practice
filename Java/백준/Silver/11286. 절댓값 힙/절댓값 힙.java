729f9644dbb9d0090ec02bd835c89b23d8a860af
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2025-01-27 11:44:52 +0900

    [Silver I] Title: 절댓값 힙, Time: 304 ms, Memory: 27180 KB -BaekjoonHub

--- Java/백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ Java/백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,5 +1,5 @@
 import java.util.*;
-        import java.io.*;
+import java.io.*;
 
 public class Main {
     static int checkArr[];
@@ -9,18 +9,16 @@ public class Main {
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(r.readLine());
-        Queue<Integer> queue = new PriorityQueue<>((a1, a2) -> { // 우선순위 큐 정렬 방식(람다식)
+        Queue<Integer> queue = new PriorityQueue<>((a1, a2) -> {
             int first = Math.abs(a1);
             int second = Math.abs(a2);
-            /* (첫 번째 요소, 두 번째 요소) 일 때, "첫 번째 요소 - 두 번째 요소" ➡️ 오름차순
-            (첫 번째 요소, 두 번째 요소) 일 때, "두 번째 요소 - 첫 번째 요소" ➡️ 내림차순 */
-            if(first == second)
+            if(first == second) {
                 return a1 > a2 ? 1 : -1;
-            else
+            }
+            else {
                 return first - second;
-
+            }
         });
-
 
         for(int i = 0; i < N; i++) {
             int num = Integer.parseInt(r.readLine());
@@ -29,7 +27,7 @@ public class Main {
             }
             else {
                 if(queue.isEmpty()) {
-                    w.write(0 + "\n");
+                    w.write(0  + "\n");
                 }
                 else {
                     w.write(queue.poll() + "\n");
