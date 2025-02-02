9ffb7372f22aeeddf1f73e91e57ef10884e5cc0f
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2025-02-02 19:50:01 +0900

    [Silver V] Title: K번째 수, Time: 1084 ms, Memory: 368560 KB -BaekjoonHub

--- Java/백준/Silver/11004. K번째 수/K번째 수.java
+++ Java/백준/Silver/11004. K번째 수/K번째 수.java
@@ -14,7 +14,7 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr);
+        quickSort(arr, 0, N-1, K-1);
 
         w.write(arr[K-1] + "\n");
 
@@ -22,4 +22,50 @@ public class Main {
         w.close();
         r.close();
     }
-}
+
+    private static void quickSort(int[] arr, int S, int E, int K) {
+        if(S < E) {
+            int pivot = partition(arr, S, E);
+            if(pivot == K)
+                return;
+            else if(pivot > K)
+                quickSort(arr, S, pivot-1, K);
+            else
+                quickSort(arr, pivot + 1, E, K);
+        }
+    }
+
+    private static int partition(int[] arr, int S, int E) {
+        if (S + 1 == E) {
+            if (arr[S] > arr[E])
+                swap(arr, S, E);
+            return E;
+        }
+        int M = (S + E) / 2;
+        swap(arr, S, M);
+        int pivot = arr[S];
+        int i = S + 1;
+        int j = E;
+        while (i <= j) {
+            while (j >= S && pivot < arr[j]) {
+                j--;
+            }
+            while (i <= E && pivot > arr[i]) {
+                i++;
+            }
+            if (i <= j) {
+                swap(arr, i++, j--);
+            }
+        }
+        arr[S] = arr[j];
+        arr[j] = pivot;
+        return j;
+    }
+
+
+        private static void swap(int[] arr, int i, int j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
