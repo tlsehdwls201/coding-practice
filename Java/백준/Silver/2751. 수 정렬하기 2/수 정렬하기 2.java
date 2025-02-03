77ed93aca5bcb761a1e662eafbe462bb8e46000e
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2025-02-03 22:23:23 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1016 ms, Memory: 114104 KB -BaekjoonHub

--- Java/백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ Java/백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,24 +1,26 @@
 import java.io.*;
 
+
+
 public class Main {
-    static int A[];
+    static int arr[];
     static int tmp[];
     public static void main(String[] args) throws IOException {
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(r.readLine());
-        A = new int[N];
+        arr = new int[N];
         tmp = new int[N];
-
         for(int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(r.readLine());
+            arr[i] = Integer.parseInt(r.readLine());
         }
 
         mergeSort(0, N-1);
 
         for(int i = 0; i < N; i++) {
-            w.write(A[i] + "\n");
+            w.write(arr[i] + "\n");
         }
+
 
 
 
@@ -30,46 +32,43 @@ public class Main {
     private static void mergeSort(int s, int e) {
         if(e - s < 1) return;
 
-
-        int m = s + (e - s) / 2;
+        int m = s + (e-s) / 2;
 
         mergeSort(s, m);
         mergeSort(m+1, e);
+
         for(int i = s; i <= e; i++) {
-            tmp[i] = A[i];
+            tmp[i] = arr[i];
         }
 
         int idx = s;
-        int i = s;
+        int i = s; // 투 포인터
         int j = m + 1;
 
         while(i <= m && j <= e) {
             if(tmp[i] > tmp[j]) {
-                A[idx] = tmp[j];
+                arr[idx] = tmp[j];
                 idx++;
                 j++;
             }
             else {
-                A[idx] = tmp[i];
+                arr[idx] = tmp[i];
                 idx++;
                 i++;
             }
         }
 
-        while(i <= m) {
-            A[idx] = tmp[i];
+        while(i <= m) { // 위 반복문이 종료되어도 i나 j가 조건에 충족하지 못한 경우, 지금과 같은 반복문을 실행
+            arr[idx] = tmp[i];
             idx++;
             i++;
         }
 
         while(j <= e) {
-            A[idx] = tmp[j];
+            arr[idx] = tmp[j];
             idx++;
             j++;
         }
-
-
     }
-
 
 }
