95e97185432b69bb84a968cd0833b801ee275c23
maxwell_pro-JIN <139670244+tlsehdwls201@users.noreply.github.com>
2025-02-05 07:50:52 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 640 ms, Memory: 142184 KB -BaekjoonHub

--- Java/백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ Java/백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -11,30 +11,31 @@ public class Main {
         StringTokenizer st = new StringTokenizer(r.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        visited = new boolean[N + 1]; // N + 1인 이유 -> 배열의 0번째 인덱스 사용 안함
-        A = new ArrayList[N + 1]; // 위와 동일
-
+        A = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
         for(int i = 1; i <= N; i++) {
             A[i] = new ArrayList<Integer>();
         }
 
-        for(int i = 0; i < M; i++) {
+        for(int i = 1; i <= M; i++) {
             st = new StringTokenizer(r.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            A[s].add(e); // 방향성이 없는 노드이기에 s번째 배열 리스트 공간에 e값을 가지는 노드를 삽입
+            A[s].add(e);
             A[e].add(s);
         }
 
-        int count = 0; // dfs의 횟수를 저장하는 변수
-        for(int i = 1; i <= N; i++) {
-            if (!visited[i]) {// 방문하지 않은 노드가 있다면? 방문 했으면 true, 안했으면 false
+        int count = 0;
+
+        for(int i = 1; i<=N; i++) {
+            if(!visited[i]) {
                 count++;
                 DFS(i);
             }
         }
 
         w.write(count + "\n");
+
 
         w.flush();
         w.close();
@@ -45,13 +46,14 @@ public class Main {
         if(visited[i])
             return;
 
-        visited[i] = true; // 방문했으니 이제 true로 전환
+        visited[i] = true;
 
-        for(int idx : A[i]) { // 노드의 인접 노드들을 추출, 확장된 for문 사용!
+        for(int idx : A[i]) {
             if(!visited[idx]) {
-                DFS(idx); // 재귀함수
+                DFS(idx);
             }
         }
     }
+
 
 }
